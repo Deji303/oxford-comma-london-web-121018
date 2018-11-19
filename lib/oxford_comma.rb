32e884247d89e4eb8d3cf572dfe8dee86dd867c2
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-19 23:16:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,8 @@
 def oxford_comma(array)
-  lastword = array.pop 
-  puts "#{array.join(", ")} and #{lastword}"
+  if array.size == 1 
+    array.pop
+  else 
+    lastword = array.pop 
+    puts "#{array.join(", ")} and #{lastword}"
+  end 
 end
